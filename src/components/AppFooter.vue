97c2c60a4c76e1c9fe7545ef924b9ccdfb673b79
FilippoBersan <filippobersan99@gmail.com>
2024-02-15 16:19:44 +0100

    feat: header main e footer components

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  name: 'Footer',
+};
+</script>
+
+<template>
+  <footer class="bg-dark">
+    <div class="container d-flex justify-content-between py-3">
+      <div>logo</div>
+      <div class="text-white">Made by Classe 110</div>
+    </div>
+  </footer>
+</template>
+
+<style scoped></style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  name: 'Header',
+};
+</script>
+
+<template>
+  <header class="bg-info">
+    <div class="container d-flex justify-content-between py-3">
+      <div>logo</div>
+      <nav>
+        <ul class="list-unstyled m-0 p-0 gap-2 d-flex">
+          <li>blog</li>
+          <li>blog</li>
+          <li>blog</li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<style scoped></style>
