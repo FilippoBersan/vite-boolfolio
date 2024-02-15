e34cee30c330f8efea4374b4a80ccd9f64184a77
FilippoBersan <filippobersan99@gmail.com>
2024-02-15 16:00:32 +0100

    feat: milestone 3

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+// Import default css
+import './scss/style.scss';
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+createApp(App).mount('#app');
